542501765662abcb3f5ef7299f1a0940c7519be0
Marc Lehmann <schmorp@schmorp.de>
2003-09-07 23:21:17 +0000

    Import of MLEHMANN/Net-FCP-0.01 from CPAN.
    gitpan-cpan-distribution: Net-FCP
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MLEHMANN/Net-FCP-0.01.tar.gz
    gitpan-cpan-author:       MLEHMANN
    gitpan-cpan-maturity:     released

---
+++ FCP.pm
@@ -0,0 +1,474 @@
+=head1 NAME
+
+Net::FCP - http://freenet.sf.net client protocol
+
+=head1 SYNOPSIS
+
+ use Net::FCP;
+
+ my $fcp = new Net::FCP;
+
+ my $ni = $fcp->txn_node_info->result;
+ my $ni = $fcp->node_info;
+
+=head1 DESCRIPTION
+
+See L<http://freenet.sourceforge.net/index.php?page=fcp> for a description
+of what the messages do. I am too lazy to document all this here.
+
+=head1 WARNING
+
+This module is alpha. While it probably won't destroy (much :) of your
+data, it currently works only with the Event module (alkthough the event
+mechanism is fully pluggable).
+
+=head2 THE Net::FCP CLASS
+
+=over 4
+
+=cut
+
+package Net::FCP;
+
+use Carp;
+use IO::Socket::INET;
+
+$VERSION = 0.01;
+
+sub event_reg_cb {
+   my ($obj) = @_;
+   require Event;
+
+   $obj->{eventdata} = Event->io (
+      fd   => $obj->{fh},
+      poll => 'r',
+      cb   => sub {
+         $obj->fh_ready;
+      },
+   );
+}
+
+sub event_unreg_cb {
+   $_[0]{eventdata}
+      and (delete $_[0]{eventdata})->cancel;
+}
+
+sub event_wait_cb {
+   Event::one_event();
+}
+
+$regcb = \&event_reg_cb;
+$unregcb = \&event_unreg_cb;
+$waitcb = \&event_wait_cb;
+
+sub touc($) {
+   local $_ = shift;
+   1 while s/((?:^|_)(?:svk|chk|uri)(?:_|$))/\U$1/;
+   s/(?:^|_)(.)/\U$1/g;
+   $_;
+}
+
+sub tolc($) {
+   local $_ = shift;
+   s/(?<=[a-z])(?=[A-Z])/_/g;
+   lc $_;
+}
+
+=item $fcp = new Net::FCP [host => $host][, port => $port]
+
+Create a new virtual FCP connection to the given host and port (default
+127.0.0.1:8481).
+
+Connections are virtual because no persistent physical connection is
+established. However, the existance of the node is checked by executing a
+C<ClientHello> transaction.
+
+=cut
+
+sub new {
+   my $class = shift;
+   my $self = bless { @_ }, $class;
+
+   $self->{host} ||= "127.0.0.1";
+   $self->{port} ||= 8481;
+
+   $self->{nodehello} = $self->txn("ClientHello")->result
+      or croak "unable to get nodehello from node\n";
+
+   $self;
+}
+
+=item $txn = $fcp->txn(type => attr => val,...)
+
+The low-level interface to transactions. Don't use it.
+
+=cut
+
+sub txn {
+   my ($self, $type, %attr) = @_;
+
+   $type = touc $type;
+
+   my $txn = "Net::FCP::Txn::$type"->new(fcp => $self, type => tolc $type, attr => \%attr);
+
+   $txn;
+}
+
+sub _txn($&) {
+   my ($name, $sub) = @_;
+   *{"$name\_txn"} = $sub;
+   *{$name} = sub { $sub->(@_)->result };
+}
+
+=item $txn = $fcp->txn_client_hello
+
+=item $nodehello = $fcp->client_hello
+
+Executes a ClientHello request and returns it's results.
+
+   {
+     max_file_size => "5f5e100",
+     protocol => "1.2",
+     node => "Fred,0.6,1.46,7050"
+   }
+
+=cut
+
+_txn client_hello => sub {
+   my ($self) = @_;
+
+   $self->txn ("client_hello");
+};
+
+=item $txn = $fcp->txn_client_info
+
+=item $nodeinfo = $fcp->client_info
+
+Executes a ClientInfo request and returns it's results.
+
+   {
+     max_file_size => "5f5e100",
+     datastore_max => "2540be400",
+     node_port => 369,
+     java_name => "Java HotSpot(_T_M) Server VM",
+     operating_system_version => "2.4.20",
+     estimated_load => 52,
+     free_memory => "5cc0148",
+     datastore_free => "5ce03400",
+     node_address => "1.2.3.4",
+     active_jobs => "1f",
+     allocated_memory => "bde0000",
+     architecture => "i386",
+     routing_time => "a5",
+     least_recent_timestamp => "f41538b878",
+     available_threads => 17,
+     datastore_used => "1f72bb000",
+     java_version => "Blackdown-1.4.1-01",
+     is_transient => "false",
+     operating_system => "Linux",
+     java_vendor => "http://www.blackdown.org/",
+     most_recent_timestamp => "f77e2cc520"
+   }
+
+=cut
+
+_txn client_info => sub {
+   my ($self) = @_;
+
+   $self->txn ("client_info");
+};
+
+=item $txn = $fcp->txn_generate_chk ($metadata, $data)
+
+=item $uri = $fcp->generate_chk ($metadata, $data)
+
+Creates a new CHK, given the metadata and data. UNTESTED.
+
+=cut
+
+_txn generate_chk => sub {
+   my ($self, $metadata, $data) = @_;
+
+   $self->txn (generate_chk => data => "$data$metadata", meta_data_length => length $metadata);
+};
+
+=item $txn = $fcp->txn_generate_svk_pair
+
+=item ($public, $private) = @{ $fcp->generate_svk_pair }
+
+Creates a new SVK pair. Returns an arrayref.
+
+   [
+     "hKs0-WDQA4pVZyMPKNFsK1zapWY",
+     "ZnmvMITaTXBMFGl4~jrjuyWxOWg"
+   ]
+
+=cut
+
+_txn generate_svk_pair => sub {
+   my ($self) = @_;
+
+   $self->txn ("generate_svk_pair");
+};
+
+=item $txn = $fcp->txn_insert_private_key ($private)
+
+=item $uri = $fcp->insert_private_key ($private)
+
+Inserts a private key. $private can be either an insert URI (must start
+with freenet:SSK@) or a raw private key (i.e. the private value you get back
+from C<generate_svk_pair>).
+
+Returns the public key.
+
+UNTESTED.
+
+=cut
+
+_txn insert_private_key => sub {
+   my ($self, $privkey) = @_;
+
+   $self->txn (invert_private_key => private => $privkey);
+};
+
+=item $txn = $fcp->txn_get_size ($uri)
+
+=item $length = $fcp->get_size ($uri)
+
+Finds and returns the size (rounded up to the nearest power of two) of the
+given document.
+
+UNTESTED.
+
+=cut
+
+_txn get_size => sub {
+   my ($self, $uri) = @_;
+
+   $self->txn (get_size => URI => $uri);
+};
+
+=item MISSING: ClientGet, ClientPut
+
+=back
+
+=head2 THE Net::FCP::Txn CLASS
+
+All requests (or transactions) are executed in a asynchroneous way (LIE:
+uploads are blocking). For each request, a C<Net::FCP::Txn> object is
+created (worse: a tcp connection is created, too).
+
+For each request there is actually a different subclass (and it's possible
+to subclass these, although of course not documented).
+
+The most interesting method is C<result>.
+
+=over 4
+
+=cut
+
+package Net::FCP::Txn;
+
+=item new arg => val,...
+
+Creates a new C<Net::FCP::Txn> object. Not normally used.
+
+=cut
+
+sub new {
+   my $class = shift;
+   my $self = bless { @_ }, $class;
+
+   my $attr = "";
+   my $data = delete $self->{attr}{data};
+
+   while (my ($k, $v) = each %{$self->{attr}}) {
+      $attr .= (Net::FCP::touc $k) . "=$v\012"
+   }
+
+   if (defined $data) {
+      $attr .= "DataLength=" . (length $data) . "\012";
+      $data = "Data\012$data";
+   } else {
+      $data = "EndMessage\012";
+   }
+
+   my $fh = new IO::Socket::INET
+      PeerHost => $self->{fcp}{host},
+      PeerPort => $self->{fcp}{port}
+      or Carp::croak "FCP::txn: unable to connect to $self->{fcp}{host}:$self->{fcp}{port}: $!\n";
+
+   binmode $fh, ":raw";
+
+   if (0) {
+      print
+         Net::FCP::touc $self->{type}, "\012",
+         $attr,
+         $data, "\012";
+   }
+
+   print $fh
+      "\x00\x00", "\x00\x02", # SESSID, PRESID
+      Net::FCP::touc $self->{type}, "\012",
+      $attr,
+      $data;
+
+   #$fh->shutdown (1); # freenet buggy?, well, it's java...
+   
+   $self->{fh} = $fh;
+   
+   $Net::FCP::regcb->($self);
+   
+   $self;
+}
+
+sub fh_ready {
+   my ($self) = @_;
+
+   if (sysread $self->{fh}, $self->{buf}, 65536, length $self->{buf}) {
+      for (;;) {
+         if ($self->{datalen}) {
+            if (length $self->{buf} >= $self->{datalen}) {
+               $self->recv_data (substr $self->{buf}, 0, $self->{datalen}, "");
+            } else {
+               last;
+            }
+         } elsif ($self->{buf} =~ s/^DataChunk\015?\012Length=(\d+)\015?\012Data\015?\012//) {
+            $self->{datalen} = $1;
+         } elsif ($self->{buf} =~ s/^([a-zA-Z]+)\015?\012(.*?)\015?\012EndMessage\015?\012//s) {
+            $self->rcv ($1, {
+                  map { my ($a, $b) = split /=/, $_, 2; ((Net::FCP::tolc $a), $b) }
+                      split /\015?\012/, $2
+            });
+         } else {
+            last;
+         }
+      }
+   } else {
+      $Net::FCP::unregcb->($self);
+      delete $self->{fh};
+      $self->eof;
+   }
+}
+
+sub rcv_data {
+   my ($self, $chunk) = @_;
+}
+
+sub rcv {
+   my ($self, $type, $attr) = @_;
+
+   $type = Net::FCP::tolc $type;
+
+   if (my $method = $self->can("rcv_$type")) {
+      $method->($self, $attr, $type);
+   } else {
+      warn "received unexpected reply type '$type' for '$self->{type}', ignoring\n";
+      $self->eof;
+   }
+}
+
+sub eof {
+   my ($self, $result) = @_;
+
+   $self->{result} = $result unless exists $self->{result};
+}
+
+=item $result = $txn->result
+
+Waits until a result is available and then returns it.
+
+This waiting is (depending on your event modul) not very efficient, as it
+is done outside the "mainloop".
+
+=cut
+
+sub result {
+   my ($self) = @_;
+
+   $Net::FCP::waitcb->() while !exists $self->{result};
+
+   return $self->{result};
+}
+
+sub DESTROY {
+   $Net::FCP::unregcb->($_[0]);
+}
+
+package Net::FCP::Txn::ClientHello;
+
+use base Net::FCP::Txn;
+
+sub rcv_node_hello {
+   my ($self, $attr) = @_;
+
+   $self->eof ($attr);
+}
+
+package Net::FCP::Txn::ClientInfo;
+
+use base Net::FCP::Txn;
+
+sub rcv_node_info {
+   my ($self, $attr) = @_;
+
+   $self->eof ($attr);
+}
+
+package Net::FCP::Txn::GenerateCHK;
+
+use base Net::FCP::Txn;
+
+sub rcv_success {
+   my ($self, $attr) = @_;
+
+   $self->eof ($attr);
+}
+
+package Net::FCP::Txn::GenerateSVKPair;
+
+use base Net::FCP::Txn;
+
+sub rcv_success {
+   my ($self, $attr) = @_;
+
+   $self->eof ([$attr->{PublicKey}, $attr->{PrivateKey}]);
+}
+
+package Net::FCP::Txn::InvertPrivateKey;
+
+use base Net::FCP::Txn;
+
+sub rcv_success {
+   my ($self, $attr) = @_;
+
+   $self->eof ($attr->{PublicKey});
+}
+
+package Net::FCP::Txn::GetSize;
+
+use base Net::FCP::Txn;
+
+sub rcv_success {
+   my ($self, $attr) = @_;
+
+   $self->eof ($attr->{Length});
+}
+
+=back
+
+=head1 SEE ALSO
+
+L<http://freenet.sf.net>.
+
+=head1 BUGS
+
+=head1 AUTHOR
+
+ Marc Lehmann <pcg@goof.com>
+ http://www.goof.com/pcg/marc/
+
+=cut
+
+1;
+
